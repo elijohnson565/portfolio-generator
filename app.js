b093fd7677330dff4b9aa8da4b9cb889f6224689
Elijah Johnson <elijohnson565@gmail.com>
2022-02-17 12:34:01 -0800

    added inquirer

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-generatePage();
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+promptUser().then(answers => console.log(answers));
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
